1ed7a6ca69d506380a87775297553f8baa9566f4
Gary Tay <garyttt@singnet.com.sg>
2020-07-30 19:11:17 +0800

    initial

---
+++ fix_jenkins_perms.sh
@@ -0,0 +1,3 @@
+UIDNUMBER=`id -u`
+[ $UIDNUMBER != "0" ] && echo "Please run this script as root or with sudo..." && exit 1
+chown -R 1000:1000 jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data
