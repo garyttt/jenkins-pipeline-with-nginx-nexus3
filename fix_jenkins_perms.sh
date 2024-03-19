ed10148bd3fcae8d707c14fb8d78fb1f1b9bab7a
Gary Tay <garyttt@singnet.com.sg>
2024-03-20 03:44:39 +0800

    Bugfix for ports and others

--- fix_jenkins_perms.sh
+++ fix_jenkins_perms.sh
@@ -1,4 +1,6 @@
 UIDNUMBER=`id -u`
 [ $UIDNUMBER != "0" ] && echo "Please run this script as root or with sudo..." && exit 1
 mkdir -p jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html
-chown -R $UIDNUMBER:docker jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html
+chmod 775 jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html
+# Please change 1000 to whatever 'sudo useradd jenkins && usermod -G docker jenkins' could provide uidNumber
+chown -R 1000:docker jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html
