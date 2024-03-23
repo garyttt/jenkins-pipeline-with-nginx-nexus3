42d50763ce18c3df26bb90a5cca99b70df2fd27b
Gary Tay <garyttt@singnet.com.sg>
2024-03-24 02:00:43 +0800

    Nexus3 to runas root, fix perms script updated

--- fix_jenkins_perms.sh
+++ fix_jenkins_perms.sh
@@ -3,4 +3,11 @@ UIDNUMBER=`id -u`
 mkdir -p jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html
 chmod 775 jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html
 # Please change 1000 to whatever 'sudo useradd jenkins && usermod -G docker jenkins' could provide uidNumber
-chown -R 1000:docker jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html
+chown -R 973:docker jenkins_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html
+
+chown -R jenkins:docker /home/jenkins /home/jenkins/.ssh
+chmod 750 /home/jenkins
+chmod 700 /home/jenkins/.ssh
+chmod 640 /home/jenkins/.ssh/authorized_keys
+chmod 640 /home/jenkins/.ssh/id_rsa.pub
+chmod 600 /home/jenkins/.ssh/id_rsa
