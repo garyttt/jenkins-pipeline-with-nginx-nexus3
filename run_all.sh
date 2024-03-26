5c0dc9540e1de49f7ccca3414fc39c1e2f51d5cf
Gary Tay <garyttt@singnet.com.sg>
2024-03-27 00:45:50 +0800

    Updates

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-UIDNUMBER=`id -u`
-# [ $UIDNUMBER -ne 1000 ] && echo "Current user must be 1000 (jenkins) to avoid permission issue" && exit 1
-# Pre-create/own folders mapped by containers to avoid permission issue later
-mkdir -p jenkns_agent01 jenkins_agent02 jenkins_config_backup jenkins_home nexus-data nginx_config nginx_html >/dev/null 2>&1
 ./stop_all.sh
 docker compose up -d 
 
