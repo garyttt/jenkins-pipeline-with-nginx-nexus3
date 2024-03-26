23dff43880440b8837461b82a1b0c0c5d04f9317
Gary Tay <garyttt@singnet.com.sg>
2024-03-27 01:10:44 +0800

    Updated fix_perms script

--- fix_jenkins_perms.sh
+++ fix_jenkins_perms.sh
@@ -4,7 +4,7 @@ mkdir -p ../jenkins_agent01 ../jenkins_agent02 ../jenkins_config_backup ../jenki
 chmod 775 ../jenkins_agent01 ../jenkins_agent02 ../jenkins_config_backup ../jenkins_home ../nexus-data nginx_config nginx_html
 # Please change 1000 to whatever uidNumber 'sudo useradd jenkins && usermod -aG docker jenkins' could provide
 # chown -R 1000:docker ../jenkins_agent01 ../jenkins_agent02 ../jenkins_config_backup ../jenkins_home
-chown -R 1001:docker ../jenkins_agent01 ../jenkins_agent02 ../jenkins_config_backup ../jenkins_home 
+chown -R 1001:docker ../jenkins_agent01 ../jenkins_agent02 ../jenkins_config_backup ../jenkins_home ../nexus-data
 
 chown -R jenkins:docker /home/jenkins /home/jenkins/.ssh
 chmod 750 /home/jenkins
